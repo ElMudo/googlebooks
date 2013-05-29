bc6031d8596566c7332da9a0445a1591dfe0caae
Conor Wyse <el.mudo@xs4all.nl>
2013-05-29 10:20:09 +0200

    Added API key and user's IP to the Google Books API calls.
    According to the documentation with the GoogleBooks gem:
    "The current Google API for Books generally requires that the requesting
    server's IP address be geolocatable. This can cause responses to be
    restricted when requested from distributed/clustered servers such as Heroku."
    
    So, the user's IP address is now passed through with the API calls and an
    API key is used in the hope that it allows more calls to be made.

--- app/controllers/volumes_controller.rb
+++ app/controllers/volumes_controller.rb
@@ -5,7 +5,8 @@ class VolumesController < ApplicationController
     # TODO: There must be a better way to ensure that @page is 1 or greater.
     @page = params[:page].to_i if params.has_key?(:page)
     @page = 1 if @page.nil? || @page < 1
-    @volumes = GoogleBooks.search('ruby on rails', {:count=>10,:page=>@page})
+    user_ip = request.remote_ip # For use on distributed servers (Heroku)
+    @volumes = GoogleBooks.search('ruby on rails', {:count=>10,:page=>@page,:api_key=>'AIzaSyChTXpKH2OtDj-sy2XJCfrwT8YQ559KMAE'}, user_ip)
     # Pagination
     @total_items = @volumes.total_items
     page_size = 10.0 # ensure floating point division when calculating page_count
